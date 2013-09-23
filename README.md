1f16b7fd59556ebe77f0d2249b450443eb39ad54
Matt Halliday <matthalliday@gmail.com>
2013-09-22 20:54:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Description
+===========
+Configuration for web apps using PHP and Ruby
+
+Requirements
+============
+
+Ubuntu
+
+Attributes
+==========
+
+None
+
+Usage
+=====
+
+Coming soon
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Matt Halliday"
+maintainer_email "matthalliday@gmail.com"
+license          "All rights reserved"
+description      "Configuration for web apps using PHP and Ruby"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
