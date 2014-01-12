a8a21765cb1e10c3a8da422116578faf5ae32797
Matt Halliday <matthalliday@gmail.com>
2014-01-11 22:50:48 -0500

    Upgrade Ruby (and gems) to version 2.1.0

--- recipes/ruby_dev.rb
+++ recipes/ruby_dev.rb
@@ -1,13 +1,13 @@
 # Install Ruby and Gems
-rbenv_ruby "2.0.0-p247"
-rbenv_global "2.0.0-p247"
+rbenv_ruby "2.1.0"
+rbenv_global "2.1.0"
 
 rbenv_gem "bundler" do
-  rbenv_version "2.0.0-p247"
+  rbenv_version "2.1.0"
 end
 
 rbenv_gem "rails" do
-  rbenv_version "2.0.0-p247"
+  rbenv_version "2.1.0"
 end
 
 package "nodejs"
